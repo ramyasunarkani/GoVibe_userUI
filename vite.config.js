e751ae3254cba58df62fd96f4361559fd6d07b41
ramyasunarkani <ramyakrishnasunarkani@gmail.com>
2025-05-16 10:47:17 +0530

    useui

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/GoVibe_userUI/',  // add this line to set base path
   plugins: [react()],
 })
